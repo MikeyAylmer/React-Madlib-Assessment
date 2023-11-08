32ec548b692a3a11605c36a6deaa007d6ff80ad4
Michael Aylmer <michaelaylmer@Michaels-MacBook-Air.local>
2023-11-08 14:53:55 -0500

    create function called addCommas that adds commas to a set of integers.

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,19 @@
-function addCommas() {}
+function addCommas(number) {
+
+    // Convert the number to a string
+    const numberStr = number.toString();
+
+    // Use a regular expression to add commas
+    // while preserving any decimal portion
+    const parts = numberStr.split('.');
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+    // Join the parts back together
+    return parts.join('.');
+}
+
+// Example usage:
+//  const numberWithCommas = addCommas(1234567.89);
+//  console.log(numberWithCommas);
 
 module.exports = addCommas;
